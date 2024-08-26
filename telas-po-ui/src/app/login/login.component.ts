7595f4c2285ff359d31cacadc173d1a6cd0030bd
Fellipe Sanches <fellipes@yahoo.com.br>
2024-08-26 17:53:08 -0300

    Início do projeto

---
+++ telas-po-ui/src/app/login/login.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { PoPageLoginModule } from '@po-ui/ng-templates';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+}
